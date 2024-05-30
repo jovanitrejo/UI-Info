529498330f2f2dd55f1d0e574197dd0088c7cc7d
Jovani Trejo <jovanitrejo@icloud.com>
2024-05-30 14:36:53 -0500

    Initial Commit

---
+++ UI Info/UI_InfoApp.swift
@@ -0,0 +1,17 @@
+//
+//  UI_InfoApp.swift
+//  UI Info
+//
+//  Created by Jovani Trejo on 5/30/24.
+//
+
+import SwiftUI
+
+@main
+struct UI_InfoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
